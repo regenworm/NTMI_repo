a9526912e0dd96fe12e9300a8c4bd977c1d7e538
Wessel Klijnsma <wesselklijnsma@gmail.com>
2016-02-04 12:16:19 +0100

    created gitignore

--- A1/ass1.py
+++ A1/ass1.py
@@ -9,40 +9,43 @@
 import re
 from optparse import OptionParser
 
+
 # reads lines from file and splits into words
 def readword(f):
-	# expression used to split words
-	exp = "[^$' '\n]*"
-	words = []
+    # expression used to split words
+    exp = "[^$' '\n]*"
+    words = []
 
-	# read all lines into lines
-	lines = f.readlines()
+    # read all lines into lines
+    lines = f.readlines()
 
-	# split all lines with exp
-	for  line in lines:
-		v = re.findall(exp, line)
+    # split all lines with exp
+    for line in lines:
+        v = re.findall(exp, line)
 
-		# save every word in words
-		words.extend(v)
-	# remove empty list items
-	words = filter(None, words)
-	return words
+        # save every word in words
+        words.extend(v)
+    # remove empty list items
+    words = filter(None, words)
+    return words
+
 
 # print highest frequency ngrams and their counts
-def printhigh(ngramtable,m):
-	# sort ngrams
-	top =  sorted(ngramtable.iteritems(), key=lambda (k,v):(v,k), reverse=True)
-	
-	# get the top m results from the sorted ngrams
-	i = 0
-	while i < m:
-		print top[i] 
-		print "\n"
-		i += 1
+def printhigh(ngramtable, m):
+    # sort ngrams
+    top = sorted(ngramtable.iteritems(), key=lambda (k, v): (v, k), reverse=True)
+
+    # get the top m results from the sorted ngrams
+    i = 0
+    while i < m:
+        print top[i]
+        print "\n"
+        i += 1
+
 
 # print sum of all frequencies for a number n
 def printsum(ngramtable):
-	print "The sum of all frequencies is %i times" % (sum(ngramtable.values()))
+    print "The sum of all frequencies is %i times" % (sum(ngramtable.values()))
 
 
 ##################
@@ -56,7 +59,7 @@ parser.add_option("-c", "--corpus", dest="file_in")
 parser.add_option("-n", dest="nth")
 parser.add_option("-m", dest="mth")
 
-(options,args) = parser.parse_args()
+(options, args) = parser.parse_args()
 
 # parameters manual editing
 file_name = options.file_in
@@ -79,35 +82,35 @@ f.close()
 # but it did not
 
 if n == 1:
-	offset = 0;
+    offset = 0;
 elif n == 2:
-	offset = 1;
+    offset = 1;
 else:
-	offset = 2;
+    offset = 2;
 
 # make ngrams and look up in dictionary
 # j is used to make ngrams
 # i is used to iterate through words
 i = 0
 ngram = ""
-while i < len(words)-offset:
-	j = n
-	# while ngram != nth order ngram keep adding words
-	while j != 0:
-		ngram = ngram + " " + words[i + n-j]
-		j = j - 1
-	# once ngram is made check if already exists
-	# if so increment count
-	if ngram in ngramtable:
-		ngramtable[ngram] += 1
-	# else initiate with 1 as startvalue
-	else:
-		ngramtable[ngram] = 1
-	# reset ngram and increment i
-	ngram = ""
-	i = i +1
+while i < len(words) - offset:
+    j = n
+    # while ngram != nth order ngram keep adding words
+    while j != 0:
+        ngram = ngram + " " + words[i + n - j]
+        j = j - 1
+    # once ngram is made check if already exists
+    # if so increment count
+    if ngram in ngramtable:
+        ngramtable[ngram] += 1
+    # else initiate with 1 as startvalue
+    else:
+        ngramtable[ngram] = 1
+    # reset ngram and increment i
+    ngram = ""
+    i = i + 1
 # part 1
-printhigh(ngramtable,m)
+printhigh(ngramtable, m)
 # part 2
 printsum(ngramtable)
 
