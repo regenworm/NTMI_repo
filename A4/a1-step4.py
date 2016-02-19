1e4db68f114d1aef1f8e12f1a7085e1f17136e45
Winand <winand@nbox.nl>
2016-02-19 16:43:25 +0100

    xti

--- A4/a1-step4.py
+++ A4/a1-step4.py
@@ -39,13 +39,15 @@ tprobs = {
           }
 
 
-def viterbi(data, language_dictionary, lexical_dictionary):
+def viterbi(data, language_dictionary, lexical_dictionary, smoothing):
     layers = [[]]
     # Compute zeroth layer
     (word, current_states) = data[0]
     for current_state in current_states:
-        layers[0].append(conditional_probability((word, current_state), lexical_dictionary))
-    
+        layers[0].append(
+            conditional_probability((word, current_state), lexical_dictionary, smoothing)
+        )
+        
     # Compute layers
     for t in range(1, len(data)):
         (word, current_states)  = data[t]
@@ -57,24 +59,25 @@ def viterbi(data, language_dictionary, lexical_dictionary):
             # Find maximum value for current state
             maximum = 0
             for i in range(0, len(previous_states)):
-                value =  layers[t-1][i] * wprobs[(word, current_state)]
-                if t > 1: value *= tprobs[(current_state, previous_states[i])]
+                value =  layers[t-1][i] * \
+                conditional_probability(
+                    (word, current_state), lexical_dictionary, smoothing
+                )
+                
+                if t > 1: value *= conditional_probability(
+                    (current_state, previous_states[i]), language_dictionary, smoothing
+                )
     
                 if value > maximum:
                     maximum = value
+                    
             layers[t].append(maximum)
     
-    print layers
-    
     path=[]
-    for t in range(len(layers)-1, 0, -1):
+    for t in range(len(layers) - 1, -1, -1):
         path = [data[t][1][layers[t].index(max(layers[t]))]] + path
     
-    print path
-    #for values in reversed(layers):
-    #    print values.index(max(values))
-    
-    #[1,2,10,11].index(max([1,2,10,11]))
+    return path
 
 # Read n-grams from file per line
 def parse_pos_file(file_handle):
@@ -97,6 +100,7 @@ def parse_pos_file(file_handle):
                         sequences.append([])
                         index+=1
                 else:
+                    chunk = re.sub('\|.*$', '', chunk)
                     sequences[index].append(tuple(chunk.rsplit('/', 1)))
     # Removing last element, if empty
     if len(sequences[index]) == 0: del sequences[index]    
@@ -107,7 +111,7 @@ def parse_pos_file(file_handle):
 def sequences_to_model_dictionaries(sequences):
     # Make dictionary which maps each unique occurrence of a bi-gram to an
     # integer.
-    language_dictionaries = (defaultdict(int), defaultdict(int))
+    language_dictionaries = [defaultdict(int), defaultdict(int)]
     lexical_dictionary = defaultdict(int)
     
     # For each found bi-gram, increment the corresponding integer of the
@@ -121,10 +125,28 @@ def sequences_to_model_dictionaries(sequences):
             tag_previous = tag
         language_dictionaries[1][(tag_previous, 'STOP')] += 1
         
-    return (language_dictionaries, (language_dictionaries[0], lexical_dictionary))
+    return (language_dictionaries, [language_dictionaries[0], lexical_dictionary])
 
 # Compute conditional probability of sequence
-def conditional_probability(bi_gram, dictionaries):
+def conditional_probability(bi_gram, dictionaries, smoothing):
+    
+    k = 5
+    
+    # In case smoothing method is set to Good-Turing and frequency of n-gram are
+    # less or equal to k.
+    if smoothing == 'yes' and dictionaries[1][bi_gram] <= k:
+        c = dictionaries[1][bi_gram]
+        N = dictionaries[2]
+        
+        if c == 0:
+            return N[1] / (N[0] + 0.0)
+        elif c <= k:
+            numerator = ((c + 1) * (N[c + 1] / (N[c] + 0.0))) - \
+                        (c * ((k + 1) * N[k + 1]) / (N[1] + 0.0))
+            denominator = 1 - (((k + 1) * N[k + 1]) / (N[1] + 0.0))
+            return numerator / (denominator + 0.0)
+        
+        
     # In case no smoothing method is set or when frequency of n-gram for 
     # Good-Turing smoothing are greater than k.
     if dictionaries[0][bi_gram[1]] == 0:
@@ -147,6 +169,8 @@ parser.add_argument('-smoothing', action='store', dest='smoothing',
                     choices=['yes', 'no'], default='no')
 parameters = parser.parse_args(sys.argv[1:])
 
+smoothing = parameters.smoothing
+
 (language_dictionaries, lexical_dictionaries) = sequences_to_model_dictionaries(
     parse_pos_file(parameters.training_set)
 )
@@ -155,9 +179,24 @@ def sequence_possible_tags(sequence, lexical_dict):
     data = []
     for word in sequence:
         tags = [pair[1] for pair in lexical_dict[1] if pair[0] == word]
-        data.append((word, tags))
+        data.append((word, language_dictionaries[0].keys()))
 
     return data
 
 # print conditional_probability(('DT', 'NN'), language_dictionaries)
-print sequence_possible_tags(['is', 'was'], lexical_dictionaries)
+data = sequence_possible_tags(['I', 'have', 'taken', 'a', 'vacation', 'last', 'year'], lexical_dictionaries)
+#data = sequence_possible_tags(['ask', 'them'], lexical_dictionaries.keys())
+
+if smoothing == 'yes':
+    language_dictionaries.append(
+    [len([i for i in language_dictionaries[1].values() if i == frequency])
+       for frequency in range(1, 8) # For range 1 to 6
+    ]
+                                 )
+    lexical_dictionaries.append(
+    [len([i for i in lexical_dictionaries[1].values() if i == frequency])
+       for frequency in range(1, 8) # For range 1 to 6
+    ]
+    )
+
+print viterbi(data, language_dictionaries, lexical_dictionaries, smoothing)
