a4b0520bdc8dbb15875448fe32ddfa23f0c136e6
Winand <winand@nbox.nl>
2016-03-03 21:21:10 +0100

    b1

---
+++ B1/b-step1.py
@@ -0,0 +1,101 @@
+'''
+    NTMI - Project Exercises
+    Part b, step 1: Binarization
+
+    Authors:
+        Alex Khawalid (10634207)
+        Wessel Klijnsma (10172432)
+        Winand Renkema (10643478)
+'''
+
+def parse(tree):
+    buffer = ''
+    level = 0
+    tag = ''
+    children = []
+    for character in tree:
+        if character == '(':
+            if level == 1:
+                if tag == '': tag = buffer.strip()
+                buffer = '('
+            elif level > 1: buffer += '('
+            level += 1
+        elif character == ')':
+            level -= 1
+            if level == 1:
+                buffer += ')'
+                children.append(buffer)
+                buffer = ''
+            elif level > 1: buffer += ')' 
+        else:
+            buffer += character
+
+    if len(children) == 0:  
+        return tuple(buffer.split(' '))
+    else:                   
+        return (tag, [parse(child) for child in children])
+
+def binarize((tag1, children)):
+   
+    if isinstance(children, list) and len(children) > 1:
+        triples = []
+        prefix = ''
+        for index in range(0, len(children)):
+            
+            (tag2, children2) = children[index]
+            triples.append((prefix, tag2, children2))
+            
+            if index == 0:
+                prefix += '@' + tag1 + '->' 
+                
+            prefix += '_' + tag2
+
+        for index in range(len(triples)-1, 0, -1):
+            if index == len(triples)-1:
+                subtree = (triples[index][0], binarize(tuple(triples[index][1:])))
+            else:
+                subtree = (triples[index][0], binarize(tuple(triples[index][1:])), subtree)
+                
+        return (tag1, binarize(children[0]), subtree)
+       
+    elif isinstance(children, list):
+        return tuple([tag1] + [binarize(child) for child in children])
+    else:
+        return (tag1, children)
+   
+def flat_tree(node):
+    
+    result = '(' + node[0]
+
+    for index in range(1, len(node)):
+        if isinstance(node[index], tuple):
+            result += ' ' + flat_tree(node[index])
+        else:
+            result += ' ' + node[index]
+
+    return result + ')'
+
+import sys, argparse
+
+# Process command line arguments
+parser = argparse.ArgumentParser(description='Tree parser')
+parser.add_argument('-input', action='store', dest='input_file',
+    type=argparse.FileType('r'), required=True)
+parser.add_argument('-output', action='store', dest='output_file', 
+    type=argparse.FileType('w'), required=True)
+parameters = parser.parse_args(sys.argv[1:])
+
+input_file  = parameters.input_file
+output_file = parameters.output_file
+
+for line in input_file.readlines():
+    
+    if len(line) > 1:
+        output_file.write(flat_tree(binarize(parse(line))))            
+    else:
+        output_file.write('\n')
+    
+output_file.write('\n')
+
+input_file.close()
+output_file.close()
---
+++ B1/test.bash
@@ -0,0 +1,5 @@
+python b-step1.py -input data/train20.txt -output train20.txt.binarized
+java -ea -Xmx1G -cp bin/ nlp.assignments.parsing.SimpleParser -train train20.txt.binarized -test data/validation20.txt
+echo "Result should be around 76.5% F1"
+#java -ea -Xmx1G -cp bin/ nlp.assignments.parsing.SimpleParser -train train20.txt.binarized -test data/test20.txt
+#echo "Result should be around 76.5% F1"
